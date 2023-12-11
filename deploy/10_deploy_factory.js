7621d4cdd1b93879e517c3169b54d8b74b211847
AlvisDu <dujc2020@gmail.com>
2023-12-11 21:25:29 +0800

    Add deploy scripts

---
+++ deploy/10_deploy_factory.js
@@ -0,0 +1,22 @@
+const colors = require('colors');
+
+module.exports = async ({ getNamedAccounts, deployments, network }) => {
+  const { deployer } = await getNamedAccounts();
+  const { deploy } = deployments;
+
+  const governor = deployer
+
+  const instance = await deploy('SftWrappedTokenFactory', {
+    contract: 'SftWrappedTokenFactory',
+    from: deployer,
+    log: true,
+  });
+  console.log(` INFO: ${colors.yellow(`SftWrappedTokenFactory`)} deployed at ${colors.green(instance.address)} on ${colors.red(network.name)}`);
+
+  const SWTFactoryFactory = await ethers.getContractFactory('SftWrappedTokenFactory');
+  const swtFactory = await SWTFactoryFactory.attach(instance.address);
+  const initTx = await swtFactory.initialize(governor);
+  console.log(` INFO: SftWrappedTokenFactory initialized at tx ${initTx.hash}`);
+};
+
+module.exports.tags = ['SftWrappedTokenFactory']
---
+++ deploy/11_deploy_impl.js
@@ -0,0 +1,16 @@
+const colors = require('colors');
+
+module.exports = async ({ getNamedAccounts, deployments, network }) => {
+  const { deployer } = await getNamedAccounts();
+  const { deploy } = deployments;
+
+  const instance = await deploy('SftWrappedToken', {
+    contract: 'SftWrappedToken',
+    from: deployer,
+    log: true,
+  });
+  console.log(` INFO: ${colors.yellow(`SftWrappedToken`)} deployed at ${colors.green(instance.address)} on ${colors.red(network.name)}`);
+
+};
+
+module.exports.tags = ['SWT_impl']
