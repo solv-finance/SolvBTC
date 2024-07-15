70af4c8d8efcb7b9f9ba3cc96976f6702fcf0aa1
Ethan Y. Tsai <yee.tsai@gmail.com>
2024-07-15 13:32:02 +1000

    set gas price of ai layer

--- deploy/utils/gasTracker.js
+++ deploy/utils/gasTracker.js
@@ -1,76 +1,113 @@
-const request = require("request")
-const { BigNumber } = require("ethers")
+const request = require("request");
+const { BigNumber } = require("ethers");
 
 const get = (url) => {
   return new Promise((resolve, reject) => {
     request(url, (err, data) => {
       if (err) {
-        reject(err)
+        reject(err);
       } else {
         if (data.statusCode == 200) {
-          resolve(data.body)
+          resolve(data.body);
         } else {
-          reject(data.statusCode)
+          reject(data.statusCode);
         }
       }
-    })
-  })
-}
+    });
+  });
+};
 const getGasPrice = (network) => {
-  const highest = process.env.gas_price_highest || 250
-  const increase = process.env.gas_price_increase || 1.1
-  const apiKey = process.env.ETHERSCAN_API_KEY
-  
+  const highest = process.env.gas_price_highest || 250;
+  const increase = process.env.gas_price_increase || 1.1;
+  const apiKey = process.env.ETHERSCAN_API_KEY;
+
   return new Promise(async (resolve, reject) => {
     let gasPrice;
 
-    if (network == 'mainnet') {
-      const response = JSON.parse(await get(`https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${apiKey}`));
-      const currentGasPrice = Number(response["result"]["FastGasPrice"]).toFixed(0);
+    if (network == "mainnet") {
+      const response = JSON.parse(
+        await get(
+          `https://api.etherscan.io/api?module=gastracker&action=gasoracle&apikey=${apiKey}`
+        )
+      );
+      const currentGasPrice = Number(
+        response["result"]["FastGasPrice"]
+      ).toFixed(0);
       if (currentGasPrice > highest) {
-        reject(`gasPrice ${currentGasPrice} gwei too high, abort, current highest ${highest} , use export gas_price_highest=xxx to set`)
+        reject(
+          `gasPrice ${currentGasPrice} gwei too high, abort, current highest ${highest} , use export gas_price_highest=xxx to set`
+        );
       }
       gasPrice = (currentGasPrice * increase * 1e9).toFixed(0);
-      console.log('CurrentGasPrice is', currentGasPrice, 'wei, increased to', gasPrice, 'wei');
-
-    } else if (network == 'dev_goerli' || network == 'goerli' || network == 'dev_sepolia' || network == 'sepolia') {
-      const chain = network == 'dev_goerli' ? 'goerli' : 'dev_sepolia' ? 'sepolia' : network;
-      const response = JSON.parse(await get(`https://api-${chain}.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=${apiKey}`));
+      console.log(
+        "CurrentGasPrice is",
+        currentGasPrice,
+        "wei, increased to",
+        gasPrice,
+        "wei"
+      );
+    } else if (
+      network == "dev_goerli" ||
+      network == "goerli" ||
+      network == "dev_sepolia" ||
+      network == "sepolia"
+    ) {
+      const chain =
+        network == "dev_goerli"
+          ? "goerli"
+          : "dev_sepolia"
+          ? "sepolia"
+          : network;
+      const response = JSON.parse(
+        await get(
+          `https://api-${chain}.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=${apiKey}`
+        )
+      );
       const currentGasPrice = Number(response["result"]).toFixed(0);
       if (currentGasPrice / 1e9 > highest) {
-        reject(`gasPrice ${gasPrice} gwei too high, abort, current highest ${highest} , use export gas_price_highest=xxx to set`)
+        reject(
+          `gasPrice ${gasPrice} gwei too high, abort, current highest ${highest} , use export gas_price_highest=xxx to set`
+        );
       }
 
-      if (currentGasPrice/1e9 < 1) {
-        gasPrice =(1e9 * increase).toFixed(0);
+      if (currentGasPrice / 1e9 < 1) {
+        gasPrice = (1e9 * increase).toFixed(0);
       } else {
         gasPrice = (currentGasPrice * increase).toFixed(0);
       }
 
-      console.log('CurrentGasPrice is', currentGasPrice, 'wei, increased to', gasPrice, 'wei');
+      console.log(
+        "CurrentGasPrice is",
+        currentGasPrice,
+        "wei, increased to",
+        gasPrice,
+        "wei"
+      );
     } else {
       const defaultPrice = {
-        'bsc': 5e9,
-        'bsctest': 10e9,
-        'dev_mumbai': 4e9,
-        'mumbai': 4e9,
-        'polygon': 90e9,
-        'development': 30e9,
-        'testnet': 30e9,
-        'mantle_testnet': 1,
-        'ailayer_test': 0.1e9,
-      }
+        bsc: 5e9,
+        bsctest: 10e9,
+        dev_mumbai: 4e9,
+        mumbai: 4e9,
+        polygon: 90e9,
+        development: 30e9,
+        testnet: 30e9,
+        mantle_testnet: 1,
+        ailayer_test: 0.1e9,
+        ailayer: 0.06e9,
+        merlin: 0.1e9,
+      };
       if (defaultPrice[network] == undefined) {
         gasPrice = 5e9; //5 gwei
       } else {
-        gasPrice = defaultPrice[network]
+        gasPrice = defaultPrice[network];
       }
     }
-    console.log(`network ${network} gasPrice ${gasPrice}`)
-    resolve(BigNumber.from('' + gasPrice));
-  })
-}
+    console.log(`network ${network} gasPrice ${gasPrice}`);
+    resolve(BigNumber.from("" + gasPrice));
+  });
+};
 
 module.exports = {
-  getGasPrice
-}
+  getGasPrice,
+};
