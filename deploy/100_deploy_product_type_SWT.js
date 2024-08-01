1132d77d08766728a481af982a7dc3ed3a2ffab1
Ethan Y. Tsai <yee.tsai@gmail.com>
2024-08-01 20:36:37 +1000

    support avalanche

--- deploy/100_deploy_product_type_SWT.js
+++ deploy/100_deploy_product_type_SWT.js
@@ -1,12 +1,12 @@
-const factoryHelpers = require('./helpers/factoryHelpers');
+const factoryHelpers = require("./helpers/factoryHelpers");
 
 module.exports = async ({ deployments }) => {
-
   const productType = "Open-end Fund Share Wrapped Token";
-  const implementation = (await deployments.get('SftWrappedTokenImpl')).address;
+  const implementation = (await deployments.get("SftWrappedTokenImpl_v1.1"))
+    .address;
 
   await factoryHelpers.setImplementation(productType, implementation);
   await factoryHelpers.deployBeacon(productType);
 };
 
-module.exports.tags = ['product_type_OFS_WrappedToken']
+module.exports.tags = ["product_type_OFS_WrappedToken"];
