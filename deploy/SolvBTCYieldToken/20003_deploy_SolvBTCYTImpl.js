1ee3f55520773079740acb7188b465b733261ef8
Ethan Y. Tsai <yee.tsai@gmail.com>
2024-09-11 14:19:20 +1000

    support SolvBTC.CORE on Core

--- deploy/SolvBTCYieldToken/20003_deploy_SolvBTCYTImpl.js
+++ deploy/SolvBTCYieldToken/20003_deploy_SolvBTCYTImpl.js
@@ -1,20 +1,20 @@
-const colors = require('colors');
+const colors = require("colors");
 
 module.exports = async ({ getNamedAccounts, deployments, network }) => {
   const { deployer } = await getNamedAccounts();
   const { deploy } = deployments;
 
-  const version = '_v2.0';
-  
+  const version = "_v2.0";
+
   const deterministicSuffix = {
-    dev_sepolia: '_dev' + version,
-    sepolia: '_tnt' + version,
-    bsctest: '_tnt' + version,
-    avax_test: '_tnt' + version,
-    merlin_test: '_tnt' + version,
-    blast_test: '_tnt' + version,
-    ailayer_test: '_tnt' + version,
-    bob_test: '_tnt' + version,
+    dev_sepolia: "_dev" + version,
+    sepolia: "_tnt" + version,
+    bsctest: "_tnt" + version,
+    avax_test: "_tnt" + version,
+    merlin_test: "_tnt" + version,
+    blast_test: "_tnt" + version,
+    ailayer_test: "_tnt" + version,
+    bob_test: "_tnt" + version,
 
     mainnet: version,
     arb: version,
@@ -25,16 +25,26 @@ module.exports = async ({ getNamedAccounts, deployments, network }) => {
     bob: version,
     avax: version,
     base: version,
-  }
+    core: version,
+  };
 
-  const instance = await deploy('SolvBTCYieldToken' + version, {
-    contract: 'SolvBTCYieldToken',
+  const instance = await deploy("SolvBTCYieldToken" + version, {
+    contract: "SolvBTCYieldToken",
     from: deployer,
     log: true,
-    deterministicDeployment: ethers.utils.keccak256(ethers.utils.toUtf8Bytes('SolvBTCYieldToken' + deterministicSuffix[network.name]))
+    deterministicDeployment: ethers.utils.keccak256(
+      ethers.utils.toUtf8Bytes(
+        "SolvBTCYieldToken" + deterministicSuffix[network.name]
+      )
+    ),
   });
-  console.log(`* INFO: ${colors.yellow(`SolvBTCYieldTokenImpl`)} deployed at ${colors.green(instance.address)} on ${colors.red(network.name)}`);
-  
+  console.log(
+    `* INFO: ${colors.yellow(
+      `SolvBTCYieldTokenImpl`
+    )} deployed at ${colors.green(instance.address)} on ${colors.red(
+      network.name
+    )}`
+  );
 };
 
-module.exports.tags = ['SolvBTCYTImpl']
+module.exports.tags = ["SolvBTCYTImpl"];
