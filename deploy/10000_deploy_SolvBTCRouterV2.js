48c2a9079d8bd084a850165170d1a2283e61b466
Ethan Y. Tsai <yee.tsai@gmail.com>
2024-11-29 23:25:13 +1100

    DeFi Vault support BTCB -> SolvBTC -> SolvBTC.BBN

--- deploy/10000_deploy_SolvBTCRouterV2.js
+++ deploy/10000_deploy_SolvBTCRouterV2.js
@@ -33,6 +33,18 @@ module.exports = async ({ getNamedAccounts, deployments, network }) => {
         "0xde178805efb7fbbf779048a5a09fb176f7c28fb87204718fcc9c6f927eea8140", // pool ID
       ],
     ],
+    bsc: [
+      [
+        "0x4aae823a6a0b376De6A78e74eCC5b079d38cBCf7", // target token - SolvBTC
+        "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c", // currency - BTCB
+        "0xafb1107b43875eb79f72e3e896933d4f96707451c3d5c32741e8e05410b321d8", // pool ID
+      ],
+      [
+        "0x1346b618dC92810EC74163e4c27004c921D446a5", // target token - SolvBTC.BBN
+        "0x4aae823a6a0b376De6A78e74eCC5b079d38cBCf7", // currency - SolvBTC
+        "1336354853777768727075850191656536701909968430898108410559797247549735288643", // pool ID
+      ],
+    ],
   };
 
   // currency, target token, path
@@ -62,6 +74,23 @@ module.exports = async ({ getNamedAccounts, deployments, network }) => {
         "0xbFEfd7c0BB235E67E314ae65bd9C4685dBE9A45E", // currency - BTCB
         "0xaDAe5fc8d830f86f53E20c8a39F7E12Ff6d4E87c", // target token - SolvBTC.ENA
         ["0x1cF0e51005971c5B78b4A8feE419832CFCCD8cf9"], // path
+      ],
+    ],
+    bsc: [
+      [
+        "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c", // currency - BTCB
+        "0x4aae823a6a0b376De6A78e74eCC5b079d38cBCf7", // target token - SolvBTC
+        [], // path
+      ],
+      [
+        "0x7130d2A12B9BCbFAe4f2634d864A1Ee1Ce3Ead9c", // currency - BTCB
+        "0x1346b618dC92810EC74163e4c27004c921D446a5", // target token - SolvBTC.BBN
+        ["0x4aae823a6a0b376De6A78e74eCC5b079d38cBCf7"], // path - SolvBTC
+      ],
+      [
+        "0x4aae823a6a0b376De6A78e74eCC5b079d38cBCf7", // currency - SolvBTC
+        "0x1346b618dC92810EC74163e4c27004c921D446a5", // target token - SolvBTC.BBN
+        [], // path
       ],
     ],
   };
