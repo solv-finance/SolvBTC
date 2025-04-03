c4b40f8c24e25577543674129baad8969af989d3
Ethan Y. Tsai <yee.tsai@gmail.com>
2025-04-03 15:33:20 +1100

    update blacklist manager

--- deploy/upgradeV3/30003_config_SolvBTC_SetBlacklistManager.js
+++ deploy/upgradeV3/30003_config_SolvBTC_SetBlacklistManager.js
@@ -1,25 +1,32 @@
-const colors = require('colors');
-const { txWait } = require('../utils/deployUtils');
-const assert = require('assert');
+const colors = require("colors");
+const { txWait } = require("../utils/deployUtils");
+const assert = require("assert");
 
 module.exports = async ({ getNamedAccounts, network }) => {
   const deployer = await hre.zksyncEthers.getWallet();
 
-  const blacklistManager = deployer;
+  const blacklistManager = "0x3295836ef0D7241b1DCE65b364A5D8a8De1B93b6";
 
-  const SolvBTCFactory = await ethers.getContractFactory('SolvBTCV3_1', deployer);
-  const solvBTCAddress = require('../SolvBTC/10099_export_SolvBTCInfos').SolvBTCInfos[network.name].erc20;
+  const SolvBTCFactory = await ethers.getContractFactory(
+    "SolvBTCV3_1",
+    deployer
+  );
+  const solvBTCAddress = require("../SolvBTC/10099_export_SolvBTCInfos")
+    .SolvBTCInfos[network.name].erc20;
   const solvBTC = SolvBTCFactory.attach(solvBTCAddress);
 
   const currentManager = await solvBTC.blacklistManager();
   if (currentManager == blacklistManager.address) {
-    console.log(`* INFO: SolvBTC current blacklist manager is already ${currentManager}`);
+    console.log(
+      `* INFO: SolvBTC current blacklist manager is already ${currentManager}`
+    );
   } else {
     const setManagerTx = await solvBTC.updateBlacklistManager(blacklistManager);
-    console.log(`* INFO: SolvBTC set blacklist manager to ${blacklistManager.address} at ${setManagerTx.hash}`);
+    console.log(
+      `* INFO: SolvBTC set blacklist manager to ${blacklistManager.address} at ${setManagerTx.hash}`
+    );
     await txWait(setManagerTx);
   }
-
 };
 
-module.exports.tags = ['SolvBTC_SetBlacklistManager'];
+module.exports.tags = ["SolvBTC_SetBlacklistManager"];
