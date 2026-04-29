09777e22b2fe1216d9d89fd10f236d99568dfb29
Ethan Y.T. <yee.tsai@gmail.com>
2026-04-29 22:24:07 +1000

    refactor: remove unused ERC3525 and ERC721 hook implementations for security hardening

--- contracts/SolvBTCRouter.sol
+++ contracts/SolvBTCRouter.sol
@@ -90,12 +90,13 @@ contract SolvBTCRouter is
             || interfaceId == type(IERC165).interfaceId;
     }
 
+/* No longer in use, deleted for security
     function onERC3525Received(
-        address, /* operator_ */
+        address, / * operator_ * /
         uint256 fromSftId_,
         uint256 toSftId_,
         uint256 value_,
-        bytes calldata /* data_ */
+        bytes calldata / * data_ * /
     ) external virtual override returns (bytes4) {
         IERC3525 openFundShare = IERC3525(msg.sender);
         uint256 openFundShareSlot = openFundShare.slotOf(toSftId_);
@@ -142,7 +143,7 @@ contract SolvBTCRouter is
         return IERC3525Receiver.onERC3525Received.selector;
     }
 
-    function onERC721Received(address, /* operator_ */ address from_, uint256 sftId_, bytes calldata /* data_ */ )
+    function onERC721Received(address, / * operator_ * / address from_, uint256 sftId_, bytes calldata / * data_ * / )
         external
         virtual
         override
@@ -242,6 +243,7 @@ contract SolvBTCRouter is
 
         emit Unstake(solvBTCAddress_, msg.sender, sft_, slot_, toSftId_, amount_);
     }
+    */
 
     function createSubscription(bytes32 poolId_, uint256 currencyAmount_)
         external
